f5860cea469b7a982108b448bbdcfa985a3dc1cf
Giulio Lombardo <giulio.lombardo@gmail.com>
2016-07-13 12:18:15 +0200

    Initial Commit

---
+++ NOPD Downloader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NOPD Downloader
+//
+//  Created by Giulio Lombardo on 13/07/16.
+//  Copyright © 2016 Giulio Lombardo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
