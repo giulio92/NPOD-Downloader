636d4dd02393c2eed9092beea8cf5936bb27934f
Giulio Lombardo <giulio.lombardo@gmail.com>
2018-09-22 13:00:50 +0200

    Add constants

---
+++ NPOD Downloader/Utility/Constants.swift
@@ -0,0 +1,59 @@
+//
+//  Constants.swift
+//  NPOD Downloader
+//
+//  Created by Giulio Lombardo on 22/09/18.
+//  Copyright © 2018 Giulio Lombardo. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+    static var bundleID: String {
+        guard let identifier: String = Bundle.main.bundleIdentifier else {
+            fatalError("Cannot find bundle identifier")
+        }
+
+        return identifier
+    }
+
+    static var applicationName: String {
+        guard let name: String = Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as? String else {
+            fatalError("Cannot find CFBundleName")
+        }
+
+        return name
+    }
+
+    static var applicationVersion: String {
+        guard let name: String = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String else {
+            fatalError("Cannot find CFBundleShortVersionString")
+        }
+
+        return name
+    }
+
+    static let gitHubPageURL: String = "https://github.com/giulio92/NPOD-Downloader"
+
+    enum Nasa {
+        private static let domain: String = "https://www.nasa.gov"
+        private static let version: String = "1"
+        static let API: String = domain + "/api/" + version
+
+        static func ubernodes() -> String {
+            return API + "/query/ubernodes.json"
+        }
+
+        static func nodeURL(id: String) -> String {
+            return API + "/record/node/" + id + ".json"
+        }
+    }
+
+    enum UserDefaultKeys {
+
+    }
+
+    enum FatalErrors {
+
+    }
+}
