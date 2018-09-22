8142a9f4c431b6e1e7c0c10306d9259b48ec894c
Giulio Lombardo <giulio.lombardo@gmail.com>
2018-09-22 13:00:38 +0200

    Add new Unit Test class

---
+++ NPOD DownloaderTests/NPOD_DownloaderTests.swift
@@ -0,0 +1,24 @@
+//
+//  NPOD_DownloaderTests.swift
+//  NPOD DownloaderTests
+//
+//  Created by Giulio Lombardo on 22/09/18.
+//  Copyright © 2018 Giulio Lombardo. All rights reserved.
+//
+
+import XCTest
+@testable import NPOD_Downloader
+
+final class NPOD_DownloaderTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of
+        // each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of
+        // each test method in the class.
+        super.tearDown()
+    }
+}
