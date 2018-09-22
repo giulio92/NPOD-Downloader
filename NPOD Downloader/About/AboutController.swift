d684575ee4079af3bb9fc711f54016feaad08880
Giulio Lombardo <giulio.lombardo@gmail.com>
2018-09-22 15:59:29 +0200

    Add AboutController

---
+++ NPOD Downloader/About/AboutController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutController.swift
+//  NPOD Downloader
+//
+//  Created by Giulio Lombardo on 22/09/18.
+//  Copyright © 2018 Giulio Lombardo. All rights reserved.
+//
+
+import Cocoa
+
+final class AboutController: NSViewController, StoryboardProtocol {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+}
