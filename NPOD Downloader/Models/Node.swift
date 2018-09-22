064f1c9fc02a238c2593f143a9d4cafc5a6ae886
Giulio Lombardo <giulio.lombardo@gmail.com>
2018-09-22 19:26:39 +0200

    Improve Model user-friendliness

--- NPOD Downloader/Models/Node.swift
+++ NPOD Downloader/Models/Node.swift
@@ -6,13 +6,33 @@
 //  Copyright © 2018 Giulio Lombardo. All rights reserved.
 //
 
+import Foundation
+
 struct Node: Decodable {
 	struct Image: Decodable {
+		private enum CodingKeys: String, CodingKey {
+			case filename
+			case timestamp
+			case uniqueID = "uuid"
+			case width
+			case height
+		}
+
 		let filename: String
 		let timestamp: String
-		let uuid: String
-		let width: String
-		let height: String
+		let uniqueID: String
+		private let width: String
+		private let height: String
+
+		var size: CGSize {
+			let formatter: NumberFormatter = NumberFormatter()
+
+			guard let width: NSNumber = formatter.number(from: width), let height: NSNumber = formatter.number(from: height) else {
+				return .zero
+			}
+
+			return CGSize(width: CGFloat(width.floatValue), height: CGFloat(height.floatValue))
+		}
 	}
 
 	struct Ubernode: Decodable {
--- NPOD Downloader/Models/Ubernodes.swift
+++ NPOD Downloader/Models/Ubernodes.swift
@@ -6,11 +6,26 @@
 //  Copyright © 2018 Giulio Lombardo. All rights reserved.
 //
 
+import Foundation
+
 struct Ubernodes: Decodable {
     struct Ubernode: Decodable {
+		private enum CodingKeys: String, CodingKey {
+			case type
+			case promoDateTime
+			case id = "nid"
+		}
+
         let type: String
-        let promoDateTime: String
-        let nid: String
+        private let promoDateTime: String // cambiare in Date
+        let id: String
+
+		var date: Date? {
+			let dateFormatter: ISO8601DateFormatter = ISO8601DateFormatter()
+			dateFormatter.formatOptions = .withInternetDateTime
+
+			return dateFormatter.date(from: promoDateTime)
+		}
     }
 
     let ubernodes: [Ubernode]
